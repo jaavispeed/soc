f0b8bfd7ccaae7058e76a2d583b23ca32e3c8ff7
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-06-25 22:28:40 -0400

    Footer and navbar

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,8 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   imports: [],
   templateUrl: './countdown.component.html',
 })
-export default class CountdownComponent {}
+export default class CountdownComponent implements OnInit, OnDestroy {
+  targetDate: Date = new Date('2026-02-01T00:00:00');
+
+  days: number = 0;
+  hours: number = 0;
+  minutes: number = 0;
+  secs: number = 0;
+
+  private intervalId: any;
+
+  ngOnInit() {
+    this.updateCountdown();
+    this.intervalId = setInterval(() => {
+      this.updateCountdown();
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  updateCountdown() {
+    const now = new Date().getTime();
+    const target = this.targetDate.getTime();
+    const diff = Math.max(0, Math.floor((target - now) / 1000)); // en segundos
+
+    this.days = Math.floor(diff / 86400);
+    this.hours = Math.floor((diff % 86400) / 3600);
+    this.minutes = Math.floor((diff % 3600) / 60);
+    this.secs = diff % 60;
+  }
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/navbar/navbar.component";
 import CountdownComponent from "../countdown/countdown.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, CountdownComponent],
+  imports: [NavbarComponent, CountdownComponent, FooterComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
