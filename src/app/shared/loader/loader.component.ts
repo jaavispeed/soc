09b04072c9774d55a0f37b3e2b8815815f29df8e
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-07-13 18:16:01 -0400

    feat: instalacion de animations

--- src/app/shared/loader/loader.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-loader',
-  imports: [],
-  templateUrl: './loader.component.html',
-})
-export class LoaderComponent { }
