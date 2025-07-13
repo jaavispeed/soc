787ef0673c67bd6b632829a2d8267b5493578c21
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-07-13 19:07:11 -0400

    feat: mejoras

--- src/app/landing/pages/landing-page/landing-page.component.ts
+++ src/app/landing/pages/landing-page/landing-page.component.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { NavbarComponent } from '../../../shared/navbar/navbar.component';
 import { CountdownComponent } from '../../components/countdown/countdown.component';
 import { FooterComponent } from '../../../shared/footer/footer.component';
+import { AuspiciadoresComponent } from "../../../shared/auspiciadores/auspiciadores.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [NavbarComponent, CountdownComponent, FooterComponent],
+  imports: [NavbarComponent, CountdownComponent, FooterComponent, AuspiciadoresComponent],
   templateUrl: './landing-page.component.html',
 })
 export default class LandingPageComponent {}
---
+++ src/app/shared/auspiciadores/auspiciadores.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auspiciadores',
+  imports: [],
+  templateUrl: './auspiciadores.component.html',
+})
+export class AuspiciadoresComponent { }
